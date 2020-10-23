ed9deea2b191aae1c70c153f5be6963d533e039d
JO3LGR550 <50847294+JO3LGR550@users.noreply.github.com>
2020-10-23 16:40:51 -0700

    Add files via upload

---
+++ FindRecurringCharacters.ps1
@@ -0,0 +1,17 @@
+﻿
+function get-firstduplicate {
+
+    Param ([string] $string) 
+    
+    $newarr = @()
+    for( $x = 0; $x -lt $string.length; $x++ ){
+        if ($newarr.Contains($string[$x]) ){
+            return $string[$x]
+        }
+        $newarr += $string[$x]
+    }
+    write-host 'There are no recurring characters in the string'
+}
+
+
+get-firstduplicate -string 'abccdrfasdgasg'
